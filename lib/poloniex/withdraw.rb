5d2eae8c9c00d8341bbe1b714f6534824a7a736c
Dmitry Shilin <dimashilin@gmail.com>
2015-08-05 13:29:33 +0300

    withdraw response reworked

--- lib/poloniex/withdraw.rb
+++ lib/poloniex/withdraw.rb
@@ -1,16 +1,31 @@
 module Poloniex
   class Withdraw < BasePoloniex
-    attr_reader :error, :success, :transaction_id, :raw, :error
+    attr_reader :error, :transaction_id, :raw, :success
 
-    def initialize(attrs = {})
+    def initialize(attrs = {}, address)
       @error = attrs['error']
-      @success = attrs['success']
-      @transaction_id = attrs['transaction_id']
+      @address = address
+      @success = @error.nil?
       @raw = attrs
+      @transaction_id = request_transaction_id(address) if @success
     end
 
     def self.send(currency, amount, address)
-      client.post('withdraw', currency: normalize_currency(currency), amount: amount, address: address)
+      response = client.post('withdraw', currency: normalize_currency(currency), amount: amount, address: address)
+      new(self.prepare_response(response), address)
+    end
+
+    private
+
+    def self.prepare_response(response)
+      response.is_a?(Array) ?
+        response.first :
+        response
+    end
+
+    def request_transaction_id(address)
+      transaction = Poloniex::Withdrawal.last_bunch_for_short_period_by_address(address).last
+      transaction.id if transaction.present?
     end
   end
 end
