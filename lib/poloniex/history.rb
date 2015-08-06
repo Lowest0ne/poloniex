f46165030b29d0bf243b263b6e481841f1fd1c92
Dmitry Shilin <dimashilin@gmail.com>
2015-08-06 13:34:45 +0300

    aligned response for sell & buy

--- lib/poloniex/history.rb
+++ lib/poloniex/history.rb
@@ -1,6 +1,6 @@
 module Poloniex
   class History < BasePoloniex
-    attr_reader :order_number, :global_trade_id, :trade_id, :type, :rate, :amount, :total, :date, :margin, :fee, :category, :raw, :error
+    attr_reader :order_number, :global_trade_id, :trade_id, :type, :rate, :amount, :total, :date, :margin, :fee, :category, :raw, :error, :fee_percent
 
     def initialize(attrs = {})
       @order_number = attrs['orderNumber']
@@ -8,10 +8,14 @@ module Poloniex
       @trade_id = attrs['tradeID']
       @type = attrs['type']
       @rate = attrs['rate']
-      @amount = attrs['amount']
-      @total = attrs['total']
+      @amount = calculate_amount(attrs)
+      @total = calculate_total(attrs)
       @date = attrs['date']
-      @fee = attrs['fee']
+      @fee_percent = attrs['fee']
+      if @total && @amount
+        @fee = calculate_fee
+        fix_total
+      end
       @category = attrs['category']
       @error = attrs['error']
       @raw = attrs
@@ -28,6 +32,28 @@ module Poloniex
 
     def self.by_order_number(order_number)
       all.map{ |key, value| value }.flatten.detect{ |order| order.order_number == order_number }
+    end
+
+    private
+
+    def fix_total
+      @total = (BigDecimal.new(@total) - BigDecimal.new(@fee)).to_s
+    end
+
+    def calculate_total(attrs = {})
+      sell_order? ?  attrs['total'] : attrs['amount']
+    end
+
+    def calculate_amount(attrs = {})
+      sell_order? ? attrs['amount'] : attrs['total']
+    end
+
+    def sell_order?
+      @type == 'sell'
+    end
+
+    def calculate_fee
+      (BigDecimal.new(@fee_percent) * BigDecimal.new(@total)).to_s
     end
   end
 end
